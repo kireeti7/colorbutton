a5ed08bd5dbd63a43ddd26b41c921b9191cd6e22
kireeti7 <dannasikireeti@gmail.com>
2025-07-30 10:33:57 +0530

    button color changes

--- src/components/colorbutton.js
+++ src/components/colorbutton.js
@@ -3,14 +3,30 @@ import { useState } from "react"
 export default function ColorButton() {
     const [color, setColor] = useState("pink")
 
-    console.log(color);
+    console.log(color,"r");
+
+    
+
     return (
         <div id="colorbutton">
             <h2>it change's the color: {color} according to the button</h2>
-            <button type="button" onClick={() => (setColor("Blue"))}>Blue</button>
-            <button onClick={() => (setColor("red"))}>Red</button>
-            <button onClick={() => (setColor("yellow"))}>Yellow</button>
-            <button onClick={() => (setColor("green"))}>Green</button>
+            <button 
+                onClick={() => setColor("Blue")}
+                style={{ backgroundColor: "blue", color: "white" }}
+            >Blue</button>
+            <button 
+                onClick={() => setColor("Red")}
+                style={{ backgroundColor: "red", color: "white" }}
+            >Red</button>
+            <button 
+                onClick={() => setColor("Yellow")}
+                style={{ backgroundColor: "yellow", color: "white" }}
+            >Yellow</button>
+            <button 
+                onClick={() => setColor("Green")}
+                style={{ backgroundColor: "green", color: "white" }}
+            >Green</button>
+            
         </div>
         
     )
