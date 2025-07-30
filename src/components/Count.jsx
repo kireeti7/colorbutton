c33e8162c9bf57170273a47dc59138497ae3982d
kireeti7 <dannasikireeti@gmail.com>
2025-07-30 12:26:23 +0530

    console updated

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Count() {
 
     const [count, setCount] = useState(0);
 
+    useEffect(() => {
+        console.log("the count is :", count)
+    }, [count]);
+
     return (
         <div>
             <h2>the count is: {count}</h2>
             <button onClick={() => setCount (count + 1)}>Increment</button>
+            {
+                console.log(count)
+            }
             <button onClick={() => setCount (count - 1)}>Decrement</button>
         </div>
+
     );
 }
